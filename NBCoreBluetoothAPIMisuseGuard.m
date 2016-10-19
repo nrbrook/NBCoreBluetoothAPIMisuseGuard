8806e65837730b51674b3a7d94c9dc475b2bb63a
Nick Brook <nrbrook@gmail.com>
2016-10-19 22:46:13 +0100

    Improved swizzling and added CBPeripheral

--- NBCoreBluetoothAPIMisuseGuard.m
+++ NBCoreBluetoothAPIMisuseGuard.m
@@ -12,24 +12,47 @@
 
 #if DEBUG
 
-static void swizzleInstance(Class class, SEL originalSelector, SEL swizzledSelector) {
-    Method originalMethod = class_getInstanceMethod(class, originalSelector);
-    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+static void swizzleForwarding(Class class, NSString * swizzledPrefix, void (^before)(NSInvocation * invocation), void (^after)(NSInvocation * invocation)) {
+    SEL selector = @selector(forwardInvocation:);
+    Method method = class_getInstanceMethod(class, selector);
     
-    BOOL didAddMethod =
-    class_addMethod(class,
-                    originalSelector,
-                    method_getImplementation(swizzledMethod),
-                    method_getTypeEncoding(swizzledMethod));
+    void(^block)(id self, NSInvocation *) = ^void(id self, NSInvocation * invocation) {
+        NSString *origSelectorName = NSStringFromSelector(invocation.selector);
+        NSString *altSelectorName = [NSString stringWithFormat:@"%@%@", swizzledPrefix, origSelectorName];
+        SEL altSelector = NSSelectorFromString(altSelectorName);
+        if([self respondsToSelector:altSelector]) {
+            if(before) {
+                before(invocation);
+            }
+            invocation.selector = altSelector;
+            [invocation invoke];
+            if(after) {
+                after(invocation);
+            }
+        }
+    };
     
-    if (didAddMethod) {
-        class_replaceMethod(class,
-                            swizzledSelector,
-                            method_getImplementation(originalMethod),
-                            method_getTypeEncoding(originalMethod));
-    } else {
-        method_exchangeImplementations(originalMethod, swizzledMethod);
+    IMP newImp = imp_implementationWithBlock(block);
+    class_addMethod(class, selector, newImp, method_getTypeEncoding(method));
+}
+
+static void removeAllFromList(Class class, NSString *toSwizzlePrefix, SEL selectors[], unsigned int numSelectors) {
+    for (int i = 0; i < numSelectors; i++) {
+        SEL selectorWithNoImplementation = sel_registerName("methodWhichMustNotExist::::");
+        IMP forwarderIMP = class_getMethodImplementation(class, selectorWithNoImplementation);
+        Method originalMethod = class_getInstanceMethod(class, selectors[i]);
+        IMP originalIMP = method_getImplementation(originalMethod);
+        const char *types = method_getTypeEncoding(originalMethod);
+        NSString *aliasSelectorName = [NSString stringWithFormat:@"%@%@", toSwizzlePrefix, NSStringFromSelector(selectors[i])];
+        class_replaceMethod(class, selectors[i], forwarderIMP, types);
+        class_addMethod(class, NSSelectorFromString(aliasSelectorName), originalIMP, types);
     }
+}
+
+static void surroundMethods(Class class, SEL *selectors, unsigned int numSelectors, void (^before)(NSInvocation * invocation), void (^after)(NSInvocation * invocation)) {
+    NSString *prefix = @"nb_";
+    swizzleForwarding(class, prefix, before, after);
+    removeAllFromList(class, prefix, selectors, numSelectors);
 }
 
 @implementation CBCentralManager (APIMisuseGuard)
@@ -37,47 +60,50 @@ static void swizzleInstance(Class class, SEL originalSelector, SEL swizzledSelec
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        swizzleInstance(class, @selector(connectPeripheral:options:), @selector(nb_connectPeripheral:options:));
-        swizzleInstance(class, @selector(retrieveConnectedPeripheralsWithServices:), @selector(nb_retrieveConnectedPeripheralsWithServices:));
-        swizzleInstance(class, @selector(cancelPeripheralConnection:), @selector(nb_cancelPeripheralConnection:));
-        swizzleInstance(class, @selector(scanForPeripheralsWithServices:options:), @selector(nb_scanForPeripheralsWithServices:options:));
-        swizzleInstance(class, @selector(stopScan), @selector(nb_stopScan));
+        SEL selectors[] = {
+            @selector(connectPeripheral:options:),
+            @selector(retrieveConnectedPeripheralsWithServices:),
+            @selector(cancelPeripheralConnection:),
+            @selector(scanForPeripheralsWithServices:options:),
+            @selector(stopScan)
+        };
+        surroundMethods(self.class, selectors, sizeof(selectors) / sizeof(SEL), ^(NSInvocation *invocation) {
+            CBCentralManager * s = invocation.target;
+            if(s.state != CBCentralManagerStatePoweredOn) {
+                        NSString *stack = [[NSThread callStackSymbols] componentsJoinedByString:@"\n"];
+                        NSAssert(NO, @"CBCentralManager was not in powered on state when a method was called. State was %ld\n\nStacktrace:\n%@", (long)s.state, stack);
+            }
+        }, nil);
     });
 }
 
-- (void)nb_stateCheck {
-    if(self.state != CBCentralManagerStatePoweredOn) {
-        NSString *stack = [[NSThread callStackSymbols] componentsJoinedByString:@"\n"];
-        NSAssert(NO, @"CBCentralManager was not in powered on state when a method was called. State was %ld\n\nStacktrace:\n%@", (long)self.state, stack);
-    }
-}
+@end
 
-#pragma mark - Method Swizzling
+@implementation CBPeripheral (APIMisuseGuard)
 
-- (void)nb_connectPeripheral:(CBPeripheral *)peripheral options:(NSDictionary<NSString *,id> *)options {
-    [self nb_stateCheck];
-    [self nb_connectPeripheral:peripheral options:options];
-}
-
-- (NSArray<CBPeripheral *> *)nb_retrieveConnectedPeripheralsWithServices:(NSArray<CBUUID *> *)serviceUUIDs {
-    [self nb_stateCheck];
-    return [self nb_retrieveConnectedPeripheralsWithServices:serviceUUIDs];
-}
-
-- (void)nb_cancelPeripheralConnection:(CBPeripheral *)peripheral {
-    [self nb_stateCheck];
-    [self nb_cancelPeripheralConnection:peripheral];
-}
-
-- (void)nb_scanForPeripheralsWithServices:(NSArray<CBUUID *> *)serviceUUIDs options:(NSDictionary<NSString *,id> *)options {
-    [self nb_stateCheck];
-    [self nb_scanForPeripheralsWithServices:serviceUUIDs options:options];
-}
-
-- (void)nb_stopScan {
-    [self nb_stateCheck];
-    [self nb_stopScan];
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        SEL selectors[] = {
+            @selector(discoverServices:),
+            @selector(discoverIncludedServices:forService:),
+            @selector(discoverCharacteristics:forService:),
+            @selector(discoverDescriptorsForCharacteristic:),
+            @selector(readValueForCharacteristic:),
+            @selector(readValueForDescriptor:),
+            @selector(writeValue:forCharacteristic:type:),
+            @selector(writeValue:forDescriptor:),
+            @selector(setNotifyValue:forCharacteristic:),
+            @selector(readRSSI)
+        };
+        surroundMethods(self.class, selectors, sizeof(selectors) / sizeof(SEL), ^(NSInvocation *invocation) {
+            CBPeripheral * s = invocation.target;
+            if(s.state != CBPeripheralStateConnected) {
+                NSString *stack = [[NSThread callStackSymbols] componentsJoinedByString:@"\n"];
+                NSAssert(NO, @"CBPeripheral was not in connected state when a method was called. State was %ld\n\nStacktrace:\n%@", (long)s.state, stack);
+            }
+        }, nil);
+    });
 }
 
 @end
@@ -87,64 +113,24 @@ static void swizzleInstance(Class class, SEL originalSelector, SEL swizzledSelec
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        swizzleInstance(class, @selector(addService:), @selector(nb_addService:));
-        swizzleInstance(class, @selector(removeService:), @selector(nb_removeService:));
-        swizzleInstance(class, @selector(removeAllServices), @selector(nb_removeAllServices));
-        swizzleInstance(class, @selector(startAdvertising:), @selector(nb_startAdvertising:));
-        swizzleInstance(class, @selector(stopAdvertising), @selector(nb_stopAdvertising));
-        swizzleInstance(class, @selector(updateValue:forCharacteristic:onSubscribedCentrals:), @selector(nb_updateValue:forCharacteristic:onSubscribedCentrals:));
-        swizzleInstance(class, @selector(respondToRequest:withResult:), @selector(nb_respondToRequest:withResult:));
+        SEL selectors[] = {
+            @selector(addService:),
+            @selector(removeService:),
+            @selector(removeAllServices),
+            @selector(startAdvertising:),
+            @selector(stopAdvertising),
+            @selector(updateValue:forCharacteristic:onSubscribedCentrals:),
+            @selector(respondToRequest:withResult:),
+            @selector(setDesiredConnectionLatency:forCentral:)
+        };
+        surroundMethods(self.class, selectors, sizeof(selectors) / sizeof(SEL), ^(NSInvocation *invocation) {
+            CBPeripheralManager * s = invocation.target;
+            if(s.state != CBPeripheralManagerStatePoweredOn) {
+                NSString *stack = [[NSThread callStackSymbols] componentsJoinedByString:@"\n"];
+                NSAssert(NO, @"CBPeripheralManager was not in powered on state when a method was called. State was %ld\n\nStacktrace:\n%@", (long)s.state, stack);
+            }
+        }, nil);
     });
-}
-
-- (void)nb_stateCheck {
-    if(self.state != CBPeripheralManagerStatePoweredOn) {
-        NSString *stack = [[NSThread callStackSymbols] componentsJoinedByString:@"\n"];
-        NSAssert(NO, @"CBPeripheralManager was not in powered on state when a method was called. State was %ld\n\nStacktrace:\n%@", (long)self.state, stack);
-    }
-}
-
-#pragma mark - Method Swizzling
-
-- (void)nb_addService:(CBMutableService *)service {
-    [self nb_stateCheck];
-    [self nb_addService:service];
-}
-
-- (void)nb_removeService:(CBMutableService *)service {
-    [self nb_stateCheck];
-    [self nb_removeService:service];
-}
-
-- (void)nb_removeAllServices {
-    [self nb_stateCheck];
-    [self nb_removeAllServices];
-}
-
-- (void)nb_startAdvertising:(NSDictionary<NSString *,id> *)advertisementData {
-    [self nb_stateCheck];
-    [self nb_startAdvertising:advertisementData];
-}
-
-- (void)nb_stopAdvertising {
-    [self nb_stateCheck];
-    [self nb_stopAdvertising];
-}
-
-- (BOOL)nb_updateValue:(NSData *)value forCharacteristic:(CBMutableCharacteristic *)characteristic onSubscribedCentrals:(NSArray<CBCentral *> *)centrals {
-    [self nb_stateCheck];
-    return [self nb_updateValue:value forCharacteristic:characteristic onSubscribedCentrals:centrals];
-}
-
-- (void)nb_respondToRequest:(CBATTRequest *)request withResult:(CBATTError)result {
-    [self nb_stateCheck];
-    [self respondToRequest:request withResult:result];
-}
-
-- (void)nb_setDesiredConnectionLatency:(CBPeripheralManagerConnectionLatency)latency forCentral:(CBCentral *)central {
-    [self nb_stateCheck];
-    [self nb_setDesiredConnectionLatency:latency forCentral:central];
 }
 
 @end
